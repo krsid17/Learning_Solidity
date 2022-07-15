47e33fe9df7ba62a6d814dae321f82a412f72b98
Sidhant <sidhant17.sk@gmail.com>
2022-07-15 18:26:31 +0530

    variables in solidity.

---
+++ variables.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity^0.8.13;
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint public num = 123;
+
+    function doSomething() view public {
+        // Local variables are not saved to the blockchain.
+        uint i = 456;
+
+        // Here are some global variables
+        uint timestamp = block.timestamp; // Current block timestamp
+        address sender = msg.sender; // address of the caller
+    }
+}
+//SPDX-License-Identifier:MIT
+
+pragma solidity^0.8.13;
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint public num = 123;
+
+    function doSomething() view public {
+        // Local variables are not saved to the blockchain.
+        uint i = 456;
+
+        // Here are some global variables
+        uint timestamp = block.timestamp; // Current block timestamp
+        address sender = msg.sender; // address of the caller
+    }
+}
